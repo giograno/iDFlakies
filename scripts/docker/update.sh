926d854f6e349aa0fb75244a7b254063aefe0ed4
Giovanni Grano <granogiovanni90@gmail.com>
2020-09-25 17:34:12 +0200

    proper fetch of the submodule

--- scripts/docker/update.sh
+++ scripts/docker/update.sh
@@ -10,5 +10,5 @@ cd /home/$SCRIPT_USERNAME/testrunner/ && git pull && /home/$SCRIPT_USERNAME/apac
 cd /home/$SCRIPT_USERNAME/$TOOL_REPO/ && git pull && /home/$SCRIPT_USERNAME/apache-maven/bin/mvn clean install -B
 
 # Fetches the module that changes the code for the performances and builds it
-cd /home/$SCRIPT_USERNAME/$TOOL_REPO/ && git submodule foreach git pull
+cd /home/$SCRIPT_USERNAME/$TOOL_REPO/ && git submodule update --init --recursive
 cd /home/$SCRIPT_USERNAME/$TOOL_REPO/BSC_TRANSFORM && /home/$SCRIPT_USERNAME/apache-maven/bin/mvn clean package
