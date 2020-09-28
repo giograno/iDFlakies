747ce67ab7e7ac846c696398861c9501052a16a4
Giovanni Grano <granogiovanni90@gmail.com>
2020-09-28 10:38:42 +0200

    script to run everything

---
+++ scripts/docker/run_new.sh
@@ -0,0 +1,15 @@
+maxTime="10000"
+rounds="20"
+csvDir="icst-dataset/comprehensive-individual-split"
+
+for entry in "$csvDir"/*
+do
+  if [[ "$entry" == *csv ]]
+  then
+    slug=$(echo $(cat ${entry}) | cut -d',' -f1 | rev | cut -d'/' -f1-2 | rev)
+    echo "Computing: $slug"
+    nohup bash create_and_run_dockers.sh $entry $rounds $maxTime > $slug.out 2>&1 &
+  else
+    continue
+  fi
+done
