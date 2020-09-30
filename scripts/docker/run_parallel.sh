fae2364ccd48bc4f7be9c71b1927e4a3b8b57de8
Giovanni Grano <granogiovanni90@gmail.com>
2020-09-30 13:52:50 +0200

    script for running in parallel

---
+++ scripts/docker/run_parallel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+maxTime="10000"
+rounds="20"
+csvDir="icst-dataset/comprehensive-individual-split"
+
+# 5 jobs in parallels
+find $csvDir -maxdepth 1 -type f -name "*.csv" | parallel -I% --jobs=5 --max-args 1 sudo bash create_and_run_dockers.sh % '>' log_{/.}.out
