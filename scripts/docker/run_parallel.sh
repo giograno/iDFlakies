e3cd730d890068fd8dd84cc252a79bef9a6c68d0
Giovanni Grano <granogiovanni90@gmail.com>
2020-10-15 11:39:29 +0200

    fix

--- scripts/docker/run_parallel.sh
+++ scripts/docker/run_parallel.sh
@@ -5,4 +5,4 @@ csvDir="icst-dataset/comprehensive-individual-split"
 noJobs=10
 
 # parallel jobs
-find $csvDir -maxdepth 1 -type f -name "*.csv" | parallel -I% --jobs=noJobs --max-args 1 sudo bash create_and_run_dockers.sh % $rounds $maxTime '>' log_{/.}.out
+find $csvDir -maxdepth 1 -type f -name "*.csv" | parallel -I% --jobs=$noJobs --max-args 1 sudo bash create_and_run_dockers.sh % $rounds $maxTime '>' log_{/.}.out
